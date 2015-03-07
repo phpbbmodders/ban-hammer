d070e4c51537c78a940b593cd906e8bea4836c00
Raphaël <Galixte@users.noreply.github.com>
2015-03-07 19:37:55 +0100

    Update for FRENCH translation for banhammer_lang.php file
    Update for FRENCH translation for banhammer_lang.php file

--- language/fr/banhammer_lang.php
+++ language/fr/banhammer_lang.php
@@ -26,14 +26,14 @@ $lang = array_merge($lang, array(
 	'BANNED_ERROR'		=> 'Il y a eu des erreurs lors du bannissement de cet utilisateur',
 	'BANNED_SUCCESS'	=> 'Cet utilisateur a été banni avec succès.',
 
-	'ERROR_BAN_EMAIL'	=> 'Echec du bannissement de l’email.',
+	'ERROR_BAN_EMAIL'	=> 'Echec du bannissement de l’e-mail.',
 	'ERROR_BAN_IP'		=> 'Echec du bannissement de l’IP.',
 	'ERROR_BAN_USER'	=> 'Echec du bannissement de l’utilisateur.',
 	'ERROR_DEL_POSTS'	=> 'Echec de la suppression des messages de l’utilisateur.',
 	'ERROR_MOVE_GROUP'	=> 'Echec du déplacement de l’utilisateur dans les groupe.',
 	'ERROR_SFS'			=> 'Arrêter le spam sur le forum',
 
-	'BH_BAN_EMAIL'			=> 'Bannir l’adresse email de cet utilisateur',
+	'BH_BAN_EMAIL'			=> 'Bannir l’adresse e-mail de cet utilisateur',
 	'BH_BAN_GIVE_REASON'	=> 'Raison affichée au membre banni',
 	'BH_BAN_IP'				=> 'Bannir l’IP de cet utilisateur',
 	'BH_BAN_IP_EXPLAIN'		=> '<strong>Nous attirons votre attention</strong> sur le fait que la plupart des utilisateurs qui ont une adresse IP dynamique ont seulement besoin de redémarrer leur routeur / modem pour en obtenir une nouvelle. Un jour un utilisateur que vous souhaitez voir sur votre forum pourrait se voir attribué cette adresse IP. Les spammeurs utilisent également des serveurs proxys ou TOR pour protéger leur anonymat rendant le bannissement d’IP inutile.',
