82b45ead47448965f312c41f55ac4e6d9ef7bf89
RMcGirr83 <rmcgirr83@gmail.com>
2017-05-17 15:19:55 -0400

    Can't use cURL if it isn't coded correctly...nub

--- event/banhammer_listener.php
+++ event/banhammer_listener.php
@@ -440,6 +440,7 @@ class banhammer_listener implements EventSubscriberInterface
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_exec($ch);
 		$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 		curl_close($ch);
 
